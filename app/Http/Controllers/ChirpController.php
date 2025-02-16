23de14dbb71836b1c11ca0ee376712f8865c713e
SYAIFUL <syaiful9507@gmail.com>
2025-02-16 23:09:02 +0700

    chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Gate;
 use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -60,16 +61,24 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp) : RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+        $validated = request()->validate([
+            'message' => 'required|string|max:255',
+        ]);
+        $chirp->update($validated);
+        return redirect(route('chirps.index'));
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Chirp $chirp)
+    public function destroy(Chirp $chirp) :  RedirectResponse
     {
-        //
+        Gate::authorize('delete', $chirp);
+        $chirp->delete();
+        return redirect(route('chirps.index'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::resource('chirps', ChirpController::class)->only(['index', 'store', 'update'])->middleware('auth', 'verified');
+Route::resource('chirps', ChirpController::class)->only(['index', 'store', 'update', 'destroy'])->middleware('auth', 'verified');
 
 
 require __DIR__.'/auth.php';
